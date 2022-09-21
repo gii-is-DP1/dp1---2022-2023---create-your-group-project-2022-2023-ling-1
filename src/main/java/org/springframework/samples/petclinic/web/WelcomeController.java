44994e385b7048f9f9227d174b906b815af05592
Marchabar <marchabar1@alum.us.es>
2022-09-21 19:41:10 +0200

    customizing the home page

--- src/main/java/org/springframework/samples/petclinic/web/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/web/WelcomeController.java
@@ -1,7 +1,8 @@
 package org.springframework.samples.petclinic.web;
 
-import java.util.Map;
+import java.util.*;
 
+import org.springframework.samples.petclinic.model.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,7 +11,36 @@ public class WelcomeController {
 	
 	
 	  @GetMapping({"/","/welcome"})
-	  public String welcome(Map<String, Object> model) {	    
+	  public String welcome(Map<String, Object> model) {
+		List<Person> persons = new ArrayList<Person>();
+		Person marco = new Person();
+		marco.setFirstName("Marco");
+		marco.setLastName(" Chasco");
+		persons.add(marco);
+		Person josemi = new Person();
+		josemi.setFirstName("Jose Miguel");
+		josemi.setLastName(" Garcia");
+		persons.add(josemi);
+		Person felix = new Person();
+		felix.setFirstName("Felix");
+		felix.setLastName(" Gudiel");
+		persons.add(felix);
+		Person enrique = new Person();
+		enrique.setFirstName("Enrique");
+		enrique.setLastName(" Cabellero");
+		persons.add(enrique);
+		Person michael = new Person();
+		michael.setFirstName("Michael");
+		michael.setLastName(" Poelzl");
+		persons.add(michael);
+		Person niclas = new Person();
+		niclas.setFirstName("Niclas");
+		niclas.setLastName(" Hulsmann");
+		persons.add(niclas);
+		model.put("persons", persons);
+		model.put("title", "DP1-2022-2023-LING-1");
+		model.put("group", "LING-1");
+			    
 
 	    return "welcome";
 	  }
